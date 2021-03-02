dd7ef68949d75b21d5553703a727ab2cab43f25d
matan-co <matandcohen@gmail.com>
2021-03-02 11:03:17 +0200

    Fixing

--- generator.sh
+++ generator.sh
@@ -3,4 +3,4 @@ mkdir out
 # Install our plugin to $GOPATH/bin
 go install .
 # Run protoc passing --hermes_out as a flag
-protoc --plugin protoc-gen-hermes --hermes_out=output example.proto
+protoc --plugin protoc-gen-hermes --hermes_out=out example.proto
